9107aaa775229b5f3bf7c4fee067765f26dfe8ae
shot9269 <110fe603@gmail.com>
2020-11-16 19:08:17 +0900

    顧客一覧のルーティング変更

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,9 +1,8 @@
 class TrainersController < ApplicationController
   def index
   end
-  
+
   def show
-    @trainer = Trainer.find(params[:id])
-    @clients = Client.includes(:trainer)
   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   get 'plans/new'
   devise_for :trainers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :trainers, only: [:show, :index]
+  resources :trainers, only: [:show,:index]
   root to: "trainers#index"
-  resources :clients, only: [:new, :create,:show]
+  resources :clients, only: [:index,:new, :create,:show]
   resources :plans, only: [:new, :create]
 end
