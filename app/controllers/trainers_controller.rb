ee4b18250c9cdca1a15581d86c9373d1524eaa86
shot9269 <110fe603@gmail.com>
2020-11-24 18:51:49 +0900

    Update trainers_controller.rb

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,8 +1,5 @@
 class TrainersController < ApplicationController 
   before_action :authenticate_trainer!
-  def index
-  end
-
   def show
     @client = Client.where(trainer_id: current_trainer.id)
   end
